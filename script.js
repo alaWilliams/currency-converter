334f0d97c01b3d2ff049fac07174cf7da9fabcbe
Ala Williams <alicja.m.williams@gmail.com>
2022-09-26 13:27:25 +0200

    main work done

---
+++ script.js
@@ -0,0 +1,35 @@
+const url = 'http://api.nbp.pl/api/exchangerates/tables/a/';
+
+const select = document.querySelector('.currencies');
+const button = document.querySelector('.btn');
+const input = document.querySelector('.input-value');
+const currencyConverted = document.querySelector('.pln-converted')
+
+
+
+const getCurrencies = () => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => convertValue(data))
+    .catch((err) => console.log(err));
+}
+
+button.addEventListener('click', getCurrencies)
+
+const convertValue = (data) => {
+  const selectVal = select.value;
+  const inputVal = input.value;
+  const mid = data[0].rates.find(element => element.code === selectVal).mid;
+  const converted = currencyConverted.innerHTML = countThePln(mid, inputVal);
+  return document.body.appendChild(converted);
+ 
+}
+
+const countThePln = (foreignCurrency, domesticCurrency) => {
+  const count = foreignCurrency * domesticCurrency
+  return count.toFixed(2);
+}
+
+
+
+
