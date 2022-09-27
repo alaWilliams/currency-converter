40745b8f8b8042f56b62719a0b08bdf73629e551
Ala Williams <alicja.m.williams@gmail.com>
2022-09-27 09:07:42 +0200

    adding https

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const url = 'http://api.nbp.pl/api/exchangerates/tables/a/';
+const url = 'https://api.nbp.pl/api/exchangerates/tables/a/';
 
 const select = document.querySelector('.currencies');
 const button = document.querySelector('.btn');
